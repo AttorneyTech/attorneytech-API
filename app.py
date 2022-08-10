76e249cb39d1d5c06e11547aa4207f53d7c2d25e
Hsieh-Te Hsieh <89285959+arieshsieh0402@users.noreply.github.com>
2022-08-11 01:53:32 +0800

    implement GET /users/{userId}

--- app.py
+++ app.py
@@ -1,53 +1,104 @@
-from flask import Flask, abort
-from flask_httpauth import HTTPBasicAuth
-from flask_restful import Resource, Api
-from werkzeug.security import generate_password_hash, check_password_hash
+from config import read_config
+from flask import abort, Flask, jsonify
+from flask_restful import Api, Resource
+import psycopg2
 
 
+# Initialize the flask framework
 app = Flask(__name__)
-auth = HTTPBasicAuth()
+app.config['JSON_SORT_KEYS'] = False
 api = Api(app)
 
 
-class Users(Resource):
+# Set the constants
+config = read_config()
+API_PORT = config['API']['PORT']
+DB_HOST = config['DATABASE']['DB_HOST']
+DB_PORT = config['DATABASE']['PORT']
+DB_NAME = config['DATABASE']['DB_NAME']
+DB_USERNAME = config['DATABASE']['DB_USERNAME']
+DB_PASSWORD = config['DATABASE']['DB_PASSWORD']
+
+
+# Construct the connection to database
+def get_db_connection():
+    conn = psycopg2.connect(
+        host=DB_HOST,
+        port=DB_PORT,
+        dbname=DB_NAME,
+        user=DB_USERNAME,
+        password=DB_PASSWORD
+    )
+    return conn
+
+
+class User(Resource):
     def get(self, userId):
-        for user in users:
-            if user['data']['id'] == userId:
-                app.logger.info(
-                    'The request of userId: %s successfully response',
-                    userId
-                )
-                return user
-        app.logger.info(
-            'The request of userId: %s is not found',
-            userId
-        )
-        return abort(404)
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute(f'''
+                    SELECT
+                    users.id, users.role, users.username,
+                    users.first_name, users.middle_name,
+                    users.last_name,
+                    events.id as event_id,
+                    cases.id as cases_id,
+                    users.email, users.phone, users.street_name,
+                    users.district, users.city, users.zip_code
+                    FROM users
+                    JOIN cases
+                    ON users.id=cases.client_id
+                    JOIN events
+                    ON cases.event_id=events.id
+                    WHERE users.id={userId};
+                    '''
+                    )
+        response = cur.fetchall()
+        cur.close()
+        conn.close()
+        if response:
+            if len(response) > 1:
+                events = []
+                cases = []
+                for row in response:
+                    if str(row[6]) not in events:
+                        events.append(str(row[6]))
+                    cases.append(str(row[7]))
+            return jsonify({
+                        "links": {
+                            "self": f"http://127.0.0.1:5000/users/{userId}"
+                            },
+                        "data": {
+                            "id": f"{userId}", "type": "users",
+                            "links": {
+                                "self":
+                                    f"http://127.0.0.1:5000/users/{userId}"
+                                },
+                            "attributes": {
+                                "role": response[0][1],
+                                "username": response[0][2],
+                                "firstName": response[0][3],
+                                "middleName": response[0][4],
+                                "lastName": response[0][5],
+                                "eventIds": events,
+                                "caseIds": cases,
+                                "email": response[0][8],
+                                "phone": response[0][9],
+                                "address": {
+                                    "addressLine1": response[0][10],
+                                    "addressLine2": response[0][11],
+                                    "city": response[0][12],
+                                    "zipCode": response[0][13]
+                                    }
+                                },
+                            },
+                        })
+        else:
+            return abort(404)
 
 
-@auth.verify_password
-def verify_password(username, password):
-    for user in users:
-        if username == user['data']['attributes']['username']:
-            if check_password_hash(
-                user['data']['attributes']['password'],
-                password
-            ):
-                app.logger.info('%s successfully logging', username)
-                return username
-            else:
-                app.logger.info('%s is fail logging', username)
-                abort(401)
-
-
-@app.route('/')
-@auth.login_required
-def index():
-    return f'Hello, {auth.current_user()}!'
-
-
-api.add_resource(Users, '/users/<string:userId>')
+api.add_resource(User, '/users/<string:userId>')
 
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(port=API_PORT)
---
+++ config.py
@@ -0,0 +1,11 @@
+import json
+
+
+def read_config():
+    """
+    Reading the configuration file
+    """
+    with open('config.json', 'r') as config_f:
+        config = json.load(config_f)
+
+    return config
