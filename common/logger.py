3b0f9a54c047c7e2b312f415066f3828768a704f
Hsieh-Te Hsieh <89285959+arieshsieh0402@users.noreply.github.com>
2022-08-26 17:29:34 +0800

    Modify logger structure

--- common/logger.py
+++ common/logger.py
@@ -1,4 +1,5 @@
 import logging
+import logging.handlers
 import os
 from datetime import datetime
 
@@ -10,35 +11,47 @@ class Logger:
     Construct the logger object
     '''
     def __init__(self):
+        self.level = config.logger_level
         self.file_path = config.logger_file_path
         self.folder_name = config.logger_folder_name.format(datetime.now())
         self.file_name = config.logger_file_name.format(datetime.now())
+        self.file_size_bytes = config.logger_file_size_bytes
+        self.file_backup_count = config.logger_file_backup_count
 
     def create_logger(self):
-        formatter = logging.Formatter(
-            '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-            datefmt='%y/%m/%d %h:%m:%s'
-        )
-        logger = logging.getLogger()
-        logger.setLevel(logging.DEBUG)
-
         if not os.path.exists(f'{self.file_path}{self.folder_name}'):
             os.makedirs(f'{self.file_path}{self.folder_name}')
 
-        # file handler
-        file_handler = logging.FileHandler(
+        # Logger format
+        format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+        datefmt = '%Y/%m/%d %H:%M:%S'
+
+        # Set file handler
+        file_handler = logging.handlers.RotatingFileHandler(
             f'{self.file_path}{self.folder_name}'
             f'/{self.file_name}',
-            'w',
-            'utf-8'
+            mode='w',
+            encoding='utf-8',
+            maxBytes=self.file_size_bytes,
+            backupCount=self.file_backup_count
         )
+        formatter = logging.Formatter(format)
         file_handler.setFormatter(formatter)
-        logger.addHandler(file_handler)
 
-        # console handler
+        # Basic config of logging
+        logging.basicConfig(
+            level=f'{self.level}',
+            format=format,
+            datefmt=datefmt
+        )
+
+        # Set handler of console
         console_handler = logging.StreamHandler()
         console_handler.setLevel(logging.DEBUG)
-        console_handler.setFormatter(formatter)
+
+        # Initialize the logger and add the handlers to it
+        logger = logging.getLogger()
+        logger.addHandler(file_handler)
         logger.addHandler(console_handler)
 
         return logger
