e9bbf6c045a362bd295ec834238e3cdeb82ca2e4
Hsieh-Te Hsieh <89285959+arieshsieh0402@users.noreply.github.com>
2022-08-31 19:48:18 +0800

    Add "key" argument to users_dao

--- db/users_dao.py
+++ db/users_dao.py
@@ -6,7 +6,7 @@ class UsersDao:
     def prepare_query(self):
         conn, cur = None, None
         try:
-            conn = conn_pool.getconn()
+            conn = conn_pool.getconn(key='users')
             cur = conn.cursor()
             cur.execute(prepare_user_query())
         except Exception as err:
@@ -15,12 +15,12 @@ class UsersDao:
         finally:
             if cur and conn:
                 cur.close()
-                conn_pool.putconn(conn=conn)
+                conn_pool.putconn(conn=conn, key='users')
 
     def get_user_by_id(self, userId):
         conn, cur = None, None
         try:
-            conn = conn_pool.getconn()
+            conn = conn_pool.getconn(key='users')
             cur = conn.cursor()
             cur.execute(f'EXECUTE get_user_by_id({userId});')
             user_raw_data = cur.fetchall()
@@ -32,8 +32,9 @@ class UsersDao:
         finally:
             if cur and conn:
                 cur.close()
-                conn_pool.putconn(conn=conn)
+                conn_pool.putconn(conn=conn, key='users')
 
 
+# Initializing the users_dao object and preload the prepare statements
 users_dao = UsersDao()
 users_dao.prepare_query()
