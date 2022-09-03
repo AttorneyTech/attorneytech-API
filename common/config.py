810a5dfb40e1aee45d3145864285045b42e797d5
Hsieh-Te Hsieh <89285959+arieshsieh0402@users.noreply.github.com>
2022-09-04 00:35:40 +0800

    Move the config file

--- common/config.py
+++ common/config.py
@@ -4,7 +4,7 @@ import traceback
 
 
 try:
-    with open('common/config.json', 'r') as config_f:
+    with open('config.json', 'r') as config_f:
         config = json.load(config_f)
 except Exception:
     traceback.print_exc()
