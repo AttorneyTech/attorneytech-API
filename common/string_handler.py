664866d2369668c69f46589546c3d709e3afccde
Hsieh-Te Hsieh <89285959+arieshsieh0402@users.noreply.github.com>
2022-09-25 00:12:52 +0800

    Fix the indentation problem

--- common/string_handler.py
+++ common/string_handler.py
@@ -4,6 +4,6 @@ from textwrap import dedent
 def string_handler(raw_string: str) -> str:
     string = dedent(
         str(raw_string)
-        ).replace('\"', '').replace('\n', '').replace('  ', '')
+    ).replace('\"', '').replace('\n', '').replace('  ', '')
 
     return string
