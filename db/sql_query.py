ce477edc744bfecf91e07abfa055c0a57c7ddb9b
Hsieh-Te Hsieh <89285959+arieshsieh0402@users.noreply.github.com>
2022-10-05 00:14:27 +0800

    Adjust the sql_query.py

--- db/sql_query.py
+++ db/sql_query.py
@@ -1,4 +1,5 @@
-def get_users_conditional_query(filter: str) -> str:
+# Define the functions of conditional statement
+def get_users_with_filter(filter: str) -> str:
     '''
     $1 - $4 are the placeholder of query parameters.
     :$1 For column users.role. Data type: varchar.
@@ -31,56 +32,67 @@ def get_users_conditional_query(filter: str) -> str:
         '''
 
 
-reusable_users_statement = {
-    'select_user_column': '''
-        SELECT
-            users.id AS user_id,
-            users.role,
-            users.username,
-            users.first_name,
-            users.middle_name,
-            users.last_name,
-            users.email,
-            users.phone,
-            users.street_name,
-            users.district,
-            users.city,
-            users.zip_code,
-            events.id AS event_id,
-            cases_users.case_id
-        FROM users
-        ''',
-    'user_join_tables': '''
-        LEFT JOIN cases_users
-            ON cases_users.user_id = users.id
-        LEFT JOIN cases
-            ON cases_users.case_id = cases.id
-        LEFT JOIN events
-            ON cases.event_id = events.id
-        '''
+reusable_function = {
+    'users': {
+        'get_users': get_users_with_filter
+    }
 }
+
+reusable_statement = {
+    'users': {
+        'select_user_column': '''
+            SELECT
+                users.id AS user_id,
+                users.role,
+                users.username,
+                users.first_name,
+                users.middle_name,
+                users.last_name,
+                users.email,
+                users.phone,
+                users.street_name,
+                users.district,
+                users.city,
+                users.zip_code,
+                events.id AS event_id,
+                cases_users.case_id
+            FROM users
+            ''',
+        'user_join_tables': '''
+            LEFT JOIN cases_users
+                ON cases_users.user_id = users.id
+            LEFT JOIN cases
+                ON cases_users.case_id = cases.id
+            LEFT JOIN events
+                ON cases.event_id = events.id
+            '''
+    }
+}
+
 prepare_statements = {
+    # GET /users/{userId}
     'get_user_by_id': f'''
         PREPARE get_user_by_id(integer) AS
-        {reusable_users_statement['select_user_column']}
-        {reusable_users_statement['user_join_tables']}
+        {reusable_statement['users']['select_user_column']}
+        {reusable_statement['users']['user_join_tables']}
         WHERE users.id = $1
         ''',
+    # GET /users
     'get_filtered_users_id': f'''
         PREPARE get_users(varchar, varchar, integer[], integer[]) AS
         SELECT DISTINCT
             users.id AS user_id
         FROM users
-        {reusable_users_statement['user_join_tables']}
-        WHERE {get_users_conditional_query('users.role')}
-            AND {get_users_conditional_query('users.city')}
-            AND {get_users_conditional_query('event_id')}
-            AND {get_users_conditional_query('case_id')};
+        {reusable_statement['users']['user_join_tables']}
+        WHERE {reusable_function['users']['get_users']('users.role')}
+            AND {reusable_function['users']['get_users']('users.city')}
+            AND {reusable_function['users']['get_users']('event_id')}
+            AND {reusable_function['users']['get_users']('case_id')};
         ''',
     'get_users': f'''
         PREPARE get_filtered_users(integer[]) AS
-        {reusable_users_statement['select_user_column']}
-        {reusable_users_statement['user_join_tables']}
+        {reusable_statement['users']['select_user_column']}
+        {reusable_statement['users']['user_join_tables']}
         WHERE users.id = ANY(ARRAY[$1])
         '''
 }
