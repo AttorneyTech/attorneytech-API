be04688bc0e206feb0babe09b694998147bc43a4
Hsieh-Te Hsieh <89285959+arieshsieh0402@users.noreply.github.com>
2022-10-07 15:54:03 +0800

    Rename dicts

--- db/sql_query.py
+++ db/sql_query.py
@@ -32,54 +32,50 @@ def get_users_with_filter(filter: str) -> str:
         '''
 
 
-reusable_function = {
-    'users': {
-        'get_users': get_users_with_filter
-    }
+reusable_func = {
+    'get_users': get_users_with_filter
 }
 
 reusable_statement = {
-    'users': {
-        'select_user_column': '''
-            SELECT
-                users.id AS user_id,
-                users.role,
-                users.username,
-                users.first_name,
-                users.middle_name,
-                users.last_name,
-                users.email,
-                users.phone,
-                users.street_name,
-                users.district,
-                users.city,
-                users.zip_code,
-                cases.event_id AS event_id,
-                cases_users.case_id
-            FROM users
-            ''',
-        'user_join_tables': '''
-            LEFT JOIN cases_users
-                ON cases_users.user_id = users.id
-            LEFT JOIN cases
-                ON cases_users.case_id = cases.id
-            '''
-    }
+    'select_user_column': '''
+        SELECT
+            users.id AS user_id,
+            users.role,
+            users.username,
+            users.first_name,
+            users.middle_name,
+            users.last_name,
+            users.email,
+            users.phone,
+            users.street_name,
+            users.district,
+            users.city,
+            users.zip_code,
+            cases.event_id AS event_id,
+            cases_users.case_id
+        FROM users
+        ''',
+    'user_join_tables': '''
+        LEFT JOIN cases_users
+            ON cases_users.user_id = users.id
+        LEFT JOIN cases
+            ON cases_users.case_id = cases.id
+        '''
 }
 
 prepare_statements = {
     # GET /users/{userId}
     'get_user_by_id': f'''
         PREPARE get_user_by_id(integer) AS
-        {reusable_statement['users']['select_user_column']}
-        {reusable_statement['users']['user_join_tables']}
+        {reusable_statement['select_user_column']}
+        {reusable_statement['user_join_tables']}
         WHERE users.id = $1
         ''',
     # GET /users
     'get_users': f'''
         PREPARE get_users(varchar, varchar, integer[], integer[]) AS
-        {reusable_statement['users']['select_user_column']}
-        {reusable_statement['users']['user_join_tables']}
+        {reusable_statement['select_user_column']}
+        {reusable_statement['user_join_tables']}
         WHERE users.id IN
             (
                 SELECT DISTINCT
@@ -88,15 +84,11 @@ prepare_statements = {
                 WHERE case_id IN (
                     SELECT cases.id
                     FROM cases
-                    WHERE {
-                        reusable_function['users']['get_users']('cases.event_id')
-                    }
-                        AND {
-                            reusable_function['users']['get_users']('cases.id')
-                        }
+                    WHERE {reusable_func['get_users']('cases.event_id')}
+                        AND {reusable_func['get_users']('cases.id')}
                 )
-                AND {reusable_function['users']['get_users']('users.role')}
-                AND {reusable_function['users']['get_users']('users.city')}
+                AND {reusable_func['get_users']('users.role')}
+                AND {reusable_func['get_users']('users.city')}
             );
         '''
 }
