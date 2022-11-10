6861c083a13af7836b8ed560f9c021ffa878a5d8
Hsieh-Te Hsieh <89285959+arieshsieh0402@users.noreply.github.com>
2022-11-10 15:23:52 +0800

    Modify modify user schema

--- schemas/users_schema.py
+++ schemas/users_schema.py
@@ -5,15 +5,20 @@ from common.openapi_loader import enums
 class UserAddressSchema(Schema):
     '''Defines the address object schema of user'''
 
-    addressLine1 = fields.Str(validate=[validate.Length(max=100)])
-    addressLine2 = fields.Str(validate=[validate.Length(max=20)])
+    addressLine1 = fields.Str(
+        validate=[validate.Length(max=100)], allow_none=True
+    )
+    addressLine2 = fields.Str(
+        validate=[validate.Length(max=20)], allow_none=True
+    )
     city = fields.Str(
         validate=[
             validate.Length(max=20),
             validate.OneOf(enums['users']['filter[city]'])
-        ]
+        ],
+        allow_none=True
     )
-    zipCode = fields.Str(validate=[validate.Length(max=10)])
+    zipCode = fields.Str(validate=[validate.Length(max=10)], allow_none=True)
 
 
 class UserAttributeSchema(Schema):
@@ -23,19 +28,31 @@ class UserAttributeSchema(Schema):
         required=True,
         validate=validate.OneOf(enums['users']['filter[role]'])
     )
-    username = fields.Str(validate=[validate.Length(max=20)])
-    firstName = fields.Str(required=True, validate=[validate.Length(max=50)])
-    middleName = fields.Str(validate=[validate.Length(max=50)])
-    lastName = fields.Str(required=True, validate=[validate.Length(max=50)])
-    eventIds = fields.List(fields.Str())
-    caseIds = fields.List(fields.Str())
+    username = fields.Str(
+        validate=[validate.Length(max=20)], allow_none=True
+    )
+    firstName = fields.Str(
+        required=True, validate=[validate.Length(max=50)]
+    )
+    middleName = fields.Str(
+        validate=[validate.Length(max=50)], allow_none=True
+    )
+    lastName = fields.Str(
+        required=True, validate=[validate.Length(max=50)]
+    )
+    eventIds = fields.List(fields.Str(allow_none=True))
+    caseIds = fields.List(fields.Str(allow_none=True))
     email = fields.Email(
         required=True,
         validate=[validate.Length(max=50), validate.Email()]
     )
-    phone = fields.Str(validate=[validate.Length(max=10)])
+    phone = fields.Str(
+        validate=[validate.Length(max=10)], allow_none=True
+    )
     address = fields.Nested(UserAddressSchema)
-    password = fields.Str(validate=[validate.Length(min=6, max=36)])
+    password = fields.Str(
+        validate=[validate.Length(min=6, max=36)], allow_none=True
+    )
 
 
 class UserDataSchema(Schema):
